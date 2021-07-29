2d85422646ec4d77930fc55c7054e33f930e660a
leoc01 <leonardocampetti@gmail.com>
2021-07-29 23:42:26 +0100

    finished

--- etchasketch.js
+++ etchasketch.js
@@ -1,24 +1,101 @@
-const blocks = document.getElementById('blocks');
+const whiteCanvas = document.getElementById('whiteCanvas');
+const sizeButton = document.getElementById('sizeButton');
+const rainbowButton = document.getElementById('rainbowButton');
+const allPixels = document.querySelectorAll('.pixel');
+
+let colorBlack = true;
 let gridSize = 16;
 
-
 function createGrid(value) {
-    blocks.style.gridTemplateColumns = `repeat(${value}, 1fr)`;
-    blocks.style.gridTemplateRows = `repeat(${value}, 1fr)`;
+    whiteCanvas.style.gridTemplateColumns = `repeat(${value}, 1fr)`;
+    whiteCanvas.style.gridTemplateRows = `repeat(${value}, 1fr)`;
 }
 
 function createPixel() {
     let pixel = document.createElement('div');
     pixel.className = 'pixel';
+    pixel.addEventListener("mouseenter", function() { 
+        paintPixel(pixel);
+    });
     return pixel;
 }
 
-function insertPixelInGrid( value ) {
-    for( let i = 0; i < value; i++ ) {
-        blocks.appendChild( createPixel() );
+function insertPixelInGrid(value) {
+    for (let i = 0; i < value; i++) {
+        whiteCanvas.appendChild(createPixel());
     }
 }
 
-createGrid( gridSize );
-insertPixelInGrid( gridSize**2 );
+function changeSize(value) {
+    if (value === 64) {
+        gridSize = resetCanvas(16);
+    } else if (value === 16) {
+        gridSize = resetCanvas(32);
+    } else if (value === 32) {
+        gridSize = resetCanvas(48);
+    } else {
+        gridSize = resetCanvas(64);
+    }
+    ;
+}
 
+function resetCanvas(value) {
+    whiteCanvas.innerHTML = '';
+    createGrid(value);
+    insertPixelInGrid(value ** 2);
+    return value;
+}
+
+function blackOrRainbow(value) {
+    gridSize = resetCanvas( gridSize );
+
+    if ( value === true ) {
+        return false;
+    }
+    return true;
+}
+
+function paintPixel(c) {
+    if( colorBlack === true ) {
+        c.style.background  = 'black';
+    } else {
+        c.style.background  = randomRainbow();
+    }
+}
+
+function randomRainbow() {
+    let rainbowColor = Math.floor(Math.random() * 7) + 1;
+
+    switch(rainbowColor) {
+        case 1:
+            color = 'violet';
+        break;
+        case 2:
+            color = 'indigo';
+        break;
+        case 3:
+            color = 'blue';
+        break;
+        case 4:
+            color = 'green';
+        break;
+        case 5:
+            color = 'yellow';
+        break;
+        case 6:
+            color = 'orange';
+        break;
+        default:
+            color = 'red';
+    }
+
+    return color;
+}
+
+window.onload = function () {
+    createGrid(gridSize);
+    insertPixelInGrid(gridSize ** 2);
+};
+
+sizeButton.addEventListener('click', function () { changeSize(gridSize) });
+rainbowButton.addEventListener('click', function() { colorBlack = blackOrRainbow(colorBlack) });
